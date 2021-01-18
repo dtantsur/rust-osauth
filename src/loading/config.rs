55ef60a1082e82f662a5381767d6e5e3cee2225a
Dmitry Tantsur <dtantsur@protonmail.com>
2021-01-18 09:48:15 +0100

    chore: update my email everywhere

--- src/loading/config.rs
+++ src/loading/config.rs
@@ -1,4 +1,4 @@
-// Copyright 2018-2021 Dmitry Tantsur <divius.inside@gmail.com>
+// Copyright 2018-2021 Dmitry Tantsur <dtantsur@protonmail.com>
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
